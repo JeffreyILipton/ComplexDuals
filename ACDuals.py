b9b4a1947f50105aeb308300a1ccee1d7d43ddbf
Jeffrey Lipton <jeffreyilipton@gmail.com>
2020-11-17 21:22:50 -0800

    Update ACDuals.py
    Moved helper functions to class constructors

--- ACDuals.py
+++ ACDuals.py
@@ -4,15 +4,48 @@ class ACDual:
     def __init__(self, real, dual):
         self.real = real
         self.dual = dual
+    
+    @classmethod
+    def FromPt(cls,pt):
+        C = pt[0]+pt[1]*complex('j')
+        return cls(complex(1.0),C)
 
+    @classmethod
+    def RotateAngle(cls,theta):
+        ''' returns a ACDual for the rotation by theta
+            system is cos(theta/2) i*sin(theta/2) because its a double cover
+        '''
+        
+        c = cos(theta/2)
+        s = sin(theta/2)
+        return cls(c+s*complex('j'),0)
+
+    @classmethod
+    def Translate(cls,D):
+        ''' returns a ACDual for the translation by D
+            system is 1, 0*i, D[]0]/2 e ,D[1] i*e because its a double cover
+        '''
+        return cls(complex(1.0),D[0]/2+D[1]/2*complex('j'))
+    
+    @classmethod
+    def RotateAroundPoint(cls,theta,D):
+        """Performs t(D)*R*T(D)inv to move the rotation point to the origin, rotate at the origin and then translate back"""
+        T = cls.Translate(D)
+        Tinv = cls.Translate([-D[0],-D[1]])
+        R = cls.RotateAngle(theta)
+        return T*R*Tinv
+        
     def __toDual(self,other):
+        '''private function to convert numbers into ACDuals, '''
         if isinstance(other, ACDual): return other
-        return ACDual(other,0)
+        if isinstance(other,complex):return ACDual(other,0)
+        return ACDual(complex(other),0)
+        
         
     def __add__(self, other1):
         other = self.__toDual(other1)
         return ACDual(self.real + other.real,
-                          self.dual + other.dual)
+                      self.dual + other.dual)
     __radd__ = __add__
 
     def __sub__(self, other1):
@@ -20,14 +53,14 @@ class ACDual:
         return ACDual(self.real - other.real,
                           self.dual - other.dual)
 
-    def __rsub__(self, other1):
-        other = self.__toDual(other1)
+    def __rsub__(self, other):
         return ACDual(other, 0) - self
-		
+
     def __truediv__(self, other1):
+        ''
         other = self.__toDual(other1)
         return ACDual(self.real/other.real,
-                          (self.dual*other.real - self.real*other.dual)/(other.real**2))
+                     (self.dual*other.real - self.real*other.dual)/(other.real**2))
 
     def __rtruediv__(self, other1):
         other = self.__toDual(other1)
@@ -55,29 +88,10 @@ class ACDual:
     def __rmatmul(self,other):
         return other*self*other.involution()
     
+    def asPt(self):
+        '''returns the XY point the dual component represents'''
+        C = self.dual
+        return C.real,C.imag
+    
     def __repr__(self):
         return repr(self.real) + ' + ' + repr(self.dual) + '*'+'\u03B5'
-        
-
-def cdToPt(cd):
-    C = cd.dual
-    return np.array([C.real,C.imag])
-
-def ptToCd(pt):
-    C = pt[0]+pt[1]*complex('j')
-    return ACDual(complex(1.0),C)
-
-def CDRotateAngle(theta):
-    c = cos(theta/2)
-    s = sin(theta/2)
-    return ACDual(c+s*complex('j'),0)
-
-def CDTranslate(D):
-    return ACDual(complex(1.0),D[0]/2+D[1]/2*complex('j'))
-	
-def CDRotateAroundPoint(theta,D):
-    d = np.array(D)
-    T = CDTranslate(d)
-    Tinv = CDTranslate(-d)
-    R = CDRotateAngle(theta)
-    return T*R*Tinv
